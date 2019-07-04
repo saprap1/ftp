ca2d23254cee5c3d19f10b69c7a95844f05dfd54
saprap1 <priyasapra.2017@gmail.com>
2019-07-03 22:59:38 -0400

    latest changes

--- scrape.py
+++ scrape.py
@@ -1,6 +1,17 @@
 '''
 scrape.py
 Scrape data from players and export to CSV file
+
+
+
+
+Fantasy football scores per week
+pro-football-reference for player game logs
+next gen stats
+calculate fantasy points from weekly game logs
+3 games at a time and predict the next 1
+
+all 2017 and 2018 data
 '''
 
 import sys
@@ -37,6 +48,19 @@ if __name__ == "__main__":
 		num_cat = len(soup.find_all("th", {"class": "thd2"}))
 
 		# Parse html of all the categories so we can add to list
+		'''
+			There is an issue here. Some category names get skipped because they are in class="thd order1 right sortable"
+			position 			category that gets skipped
+			QUARTERBACK			YDS
+			RUNNING_BACK		YDS/G
+			WIDE_RECEIVER		REC
+			TIGHT_END			REC
+			DEFENSIVE_LINEMAN	COMB
+			LINEBACKER			COMB
+			DEFENSIVE_BACK		COMB
+			KICKOFF_KICKER		OOB
+			
+		'''
 		###### THIS SKIPS THE "YDS" CATEGORY BECAUSE THAT HAS CLASS "thd2 order1 right sortable" ########
 		categories = soup.find_all("th", {"class": "thd2 right sortable"})
 
@@ -47,6 +71,9 @@ if __name__ == "__main__":
 		# Add all the different category headers to a list -- initialize with the standard categories that are for every position
 		cat_headers = ["Rk", "Player", "Team", "Pos"]
 		for item in categories:
+
+			# check class here with an if statement
+
 			cat_headers.append(item.find("a").text)
 		# print(cat_headers)
 
@@ -64,6 +91,9 @@ if __name__ == "__main__":
 		
 
 		# Add each player with all the stats for that player
+
+		## So far, this collects data for the top 50 players in each position
+
 		for wrapper in players:
 			count += 1
 			if count == reset:

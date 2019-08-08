e8dec06ac002e2d9ba8c584096a4a29b96e70f33
saprap1 <priyasapra.2017@gmail.com>
2019-08-08 10:40:39 -0400

    Adds more info player spreadsheets
    Now has "last name" "first name" "position"
    Tried to fix try_link, but still fails if 2 players have the same name, but only one is active
    Also tried to get all players of 1 position into the same spreadsheet so we have QB_data2018, RB_data2018, etc.

--- read_through_names.py
+++ read_through_names.py
@@ -9,13 +9,28 @@ import openpyxl
 from bs4 import BeautifulSoup
 import requests
 
-def try_link(linkfirst, version, linksecond):
+def try_link(linkfirst, version, linksecond, fullName):
+
+
+    if version == 4:
+        return "na"
     link = linkfirst + str(version) + linksecond
     a = requests.get(link)
     soup = BeautifulSoup(a.text, 'lxml')
-    if version == 4:
-        return "na"
-    print("try_link", link)
+
+    '''
+    # Check if this is the right player's page
+    # (players with different names might have earlier link versions)
+    title = soup.find("title")
+    print("FROM PAGE:", title.text, "LOOKING FOR", fullName)
+    if (title.text).find(fullName) != -1:
+        print("try_link: found correct page...", link)
+        # well... I found out that there are multiple Josh Allen's
+        return
+    '''
+    
+    '''
+    # print("try_link", link)
     try:
         # This will fail if a 2018 gamelog doesn't exist
         tb = soup.find("tbody")
@@ -25,7 +40,7 @@ def try_link(linkfirst, version, linksecond):
         # increase the version and try again
         version += 1
         try_link(linkfirst, version, linksecond)
-
+    '''
 
 def main():
     book2 = openpyxl.Workbook()
@@ -35,20 +50,39 @@ def main():
 
     count = 0
 
+    # prev_position = sheet2.cell(row=2, column=2).value  
+    
+
     for i in range (2, row_count2+1):
 
-        #if count == 10:
-         #   break
 
+        position = sheet2.cell(row=i, column=2).value
         wb = openpyxl.Workbook()
-        # dest_filename = 'data_2018.xlsx'
         sheet = wb.active
+
+        '''
+        # dest_filename = firstName + "_" + lastName + year + ".xlsx"
+        if (position.find('/') != -1):
+            pos_split = position.split("/")
+            position = pos_split[0] + "-" + pos_split[1]
+
+        # only finish up the workbook if there is a NEW position
+        if (prev_position != position):
+            dest_filename = position + "_data" + year + ".xlsx"
+            wb.save(filename = dest_filename)
+            prev_position = position
+            # start a new workbook
+            wb = openpyxl.Workbook()
+            sheet = wb.active
+        '''
+    
+        if count == 10:
+           break
 
         s = sheet2.cell(row=i, column=1).value
         x = s.split(",")
         firstName = x[-1][1:]
         lastName = x[0]
-        #https://www.pro-football-reference.com/players/B/BradTo00/gamelog/2018/
         version = 0
         year = "2018"
         link = "https://www.pro-football-reference.com/players/" + lastName[0] + "/" + lastName[:4] + firstName[:2] + "0" + str(version) + "/gamelog/" + year + "/"
@@ -67,30 +101,24 @@ def main():
             # Here, need to handle 2 cases..
             #   - either player is new and doesn't have 2018 data
             #   - or player has a different version # due to sharing a URL
-
-            # Gets an error here bc the player page lied to me and indicated a player as active by bolding the font
-            # but the player's last active season is 2017, not 2018. We don't need this data, so we can skip it.
-            # print(count, "ERROR with", link)
             
-            
-            # link = try_link("https://www.pro-football-reference.com/players/" + lastName[0] + "/" + lastName[:4] + firstName[:2] + "0", version, "/gamelog/" + year + "/")
-            # if link == "na":
-                # continue
-            # a = requests.get(link)
-            # soup = BeautifulSoup(a.text, 'lxml')
-            # tb = soup.find("tbody")
-            # row = tb.findAll("tr")
-            
-
-            # version += 1
-            print("continuing..")
-            count +=1
+            # fullName = firstName + " " + lastName
+            # link = try_link("https://www.pro-football-reference.com/players/" + lastName[0] + "/" + lastName[:4] + firstName[:2] + "0", version, "/gamelog/" + year + "/", fullName)
+            '''
+            if link == "na":
+                continue
+            a = requests.get(link)
+            soup = BeautifulSoup(a.text, 'lxml')
+            tb = soup.find("tbody")
+            row = tb.findAll("tr")
+            '''
+            print("skipping for " + firstName + " " + lastName + "...")
             continue
 
         for x in row:
             items = x.findAll("td")
             counter = 0
-            append_row = [link]
+            append_row = [lastName, firstName, position]
 
             for y in items:
                 if(counter == 0):
@@ -99,12 +127,12 @@ def main():
 
             sheet.append(append_row)
 
+        
         dest_filename = firstName + "_" + lastName + year + ".xlsx"
         wb.save(filename = dest_filename)
 
+        
+
         count +=1
-        #page += p.a["href"]
-        #s = x[-1][1:] + " " + x[0]
-        #print(s)
         
 main()

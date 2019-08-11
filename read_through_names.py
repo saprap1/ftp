cec74f656e909bc6268d697793150e290b8cfddb
saprap1 <priyasapra.2017@gmail.com>
2019-08-11 11:56:58 -0400

    moved points calculation into a separate function (see entire commit message for bug details)
    There are some bugs in calculating points. I just focused on the tight ends for now. Some TEs have a whole
    section for passing, some are missing columns. These are things that cause items[] to fail.
    I specified some examples in the code.

--- read_through_names.py
+++ read_through_names.py
@@ -10,37 +10,66 @@ from bs4 import BeautifulSoup
 import requests
 
 def try_link(linkfirst, version, linksecond, fullName):
+    return
 
-
-    if version == 4:
-        return "na"
-    link = linkfirst + str(version) + linksecond
-    a = requests.get(link)
-    soup = BeautifulSoup(a.text, 'lxml')
-
-    '''
-    # Check if this is the right player's page
-    # (players with different names might have earlier link versions)
-    title = soup.find("title")
-    print("FROM PAGE:", title.text, "LOOKING FOR", fullName)
-    if (title.text).find(fullName) != -1:
-        print("try_link: found correct page...", link)
-        # well... I found out that there are multiple Josh Allen's
-        return
-    '''
-    
-    '''
-    # print("try_link", link)
+'''
+    fantasy points calculation by position and returns the total calculated for each game
+    Since items is a list of <td> tags, we can just get the text from that item in the list
+'''
+def calculatePoints(position, items):
+    points = 0
     try:
-        # This will fail if a 2018 gamelog doesn't exist
-        tb = soup.find("tbody")
-        row = tb.findAll("tr")
-        return link
+        if position == "QB":
+            yds_pass = float(items[13].text)
+            td_pass = float(items[14].text)
+            intercep = float(items[15].text)
+            rush_yds = float(items[22].text)
+            rush_td = float(items[24].text)
+            #fumb = float(items[34])
+            points = yds_pass/25 + td_pass*4 + intercep*-2 + rush_yds/10 + rush_td*6 
+            #fumb*-2
+        elif position == "TE":
+            # Is there a way to get the information by the attribute data-stat? that seems to be the only consistent thing among players
+            # Works for: Rob Gronkowski
+            # Fails for: Delanie Walker (only 1 row, not fumbles), Eric Ebron (has an entire section for passing unlike gronk)
+            print(items)
+            print('yds_rec', items[10].text, 'td', items[12].text, 'fmb', items[17].text)
+            # print(items)
+            yds_rec = float(items[12].text)
+            td = float(items[14].text)
+            fmb = float(items[17].text)
+            points = yds_rec/10 + td*6 + fmb*-2
+        elif position == "WR":
+            rec_yds = float(items[12].text)
+            rec_td = float(items[14].text)
+            rush_yds = float(items[18].text)
+            rush_td = float(items[20].text)
+            fmb = float(items[23].text)
+            points = rec_yds/10 + rec_td*6 + rush_yds/10 + rush_td*6 + fmb*-2
+        elif position == "RB":
+            rush_yds = float(items[11].text)
+            rush_td = float(items[13].text)
+            rec_yds = float(items[16].text)
+            rec_td = float(items[18].text)
+            fmb = float(items[23].text)
+            points = rush_yds/10 + rush_td*6 + rec_yds/10 + rec_td*6 + fmb*-2
+        elif position == "K":
+            #points calculation
+            xpm = float(items[10].text)
+            xpa = float(items[11].text)
+            fgm = float(items[13].text)
+            fga = float(items[14].text)
+            pts = float(items[17].text)
+            points = (xpm- (xpa-xpm)) + (fgm*3 - (fga-fgm)) 
+        else:
+            #wr/te/rb calc
+            points = 0
     except:
-        # increase the version and try again
-        version += 1
-        try_link(linkfirst, version, linksecond)
-    '''
+        # print("skipping points calculation for...", firstName, lastName)
+        print("skip")
+        # continue
+    # print(points)
+    return points
 
 def main():
     book2 = openpyxl.Workbook()
@@ -52,7 +81,6 @@ def main():
 
     # prev_position = sheet2.cell(row=2, column=2).value  
     
-
     for i in range (2, row_count2+1):
 
 
@@ -60,24 +88,10 @@ def main():
         wb = openpyxl.Workbook()
         sheet = wb.active
 
-        '''
-        # dest_filename = firstName + "_" + lastName + year + ".xlsx"
-        if (position.find('/') != -1):
-            pos_split = position.split("/")
-            position = pos_split[0] + "-" + pos_split[1]
-
-        # only finish up the workbook if there is a NEW position
-        if (prev_position != position):
-            dest_filename = position + "_data" + year + ".xlsx"
-            wb.save(filename = dest_filename)
-            prev_position = position
-            # start a new workbook
-            wb = openpyxl.Workbook()
-            sheet = wb.active
-        '''
-    
-        if count == 4:
+        #########debug stuff#############
+        if count == 1:
            break
+        #################################
 
         s = sheet2.cell(row=i, column=1).value
         x = s.split(",")
@@ -89,29 +103,38 @@ def main():
         #make soup request for data to formulate spreadsheet
         #open a spreadsheet, add fields, save it
         
+        ########################more debug stuff#######################
+        # link = "https://www.pro-football-reference.com/players/G/GronRo00/gamelog/2018/"
+        # firstName = "Rob"
+        # lastName = "Gronkowski"
+
+        # link = "https://www.pro-football-reference.com/players/W/WalkHu00/gamelog/2018/"
+        # firstName = "Delanie"
+        # lastName = "Walker"
+
+        # link = "https://www.pro-football-reference.com/players/E/EbroEr00/gamelog/2018/"
+        # firstName = "Eric"
+        # lastName = "Ebron"
+
+        position = "TE"
+        ###############################################################
+
         a = requests.get(link)
         soup = BeautifulSoup(a.text, 'lxml')
 
-        print("got here", link)
+        # print("got here", link)
 
         try:
             tb = soup.find("tbody")
             row = tb.findAll("tr")
+
+            # print(row)
+
         except:
             # Here, need to handle 2 cases..
             #   - either player is new and doesn't have 2018 data
             #   - or player has a different version # due to sharing a URL
-            
-            # fullName = firstName + " " + lastName
-            # link = try_link("https://www.pro-football-reference.com/players/" + lastName[0] + "/" + lastName[:4] + firstName[:2] + "0", version, "/gamelog/" + year + "/", fullName)
-            '''
-            if link == "na":
-                continue
-            a = requests.get(link)
-            soup = BeautifulSoup(a.text, 'lxml')
-            tb = soup.find("tbody")
-            row = tb.findAll("tr")
-            '''
+        
             print("skipping for " + firstName + " " + lastName + "...")
             continue
 
@@ -120,76 +143,18 @@ def main():
             counter = 0
             append_row = [lastName, firstName, position]
 
-            # print(items)
-
             for y in items:
                 if(counter == 0):
                     if(y.string!="None"):
                         append_row.append(y.text)
             
-            '''
-            fantasy points calculation by position
-            Since items is a list of <td> tags, we can just get the text from that item in the list
-
-            Some players don't have *all* the data (if u look here:got here https://www.pro-football-reference.com/players/B/BarkMa00/gamelog/2018/
-                             you'll see that there is no 24th column)
-                    -- column names are not consistent for all players...
-
-            '''
-            # print(x)
-            points = 0
-            try:
-                if position == "QB":
-                    yds_pass = float(items[13].text)
-                    td_pass = float(items[14].text)
-                    intercep = float(items[15].text)
-                    rush_yds = float(items[22].text)
-                    rush_td = float(items[24].text)
-                    #fumb = float(items[34])
-                    points = yds_pass/25 + td_pass*4 + intercep*-2 + rush_yds/10 + rush_td*6 
-                    #fumb*-2
-                elif position == "TE":
-                    yds_rec = float(items[12].text)
-                    td = float(items[14].text)
-                    fmb = float(items[19].text)
-                    points = yds_rec/10 + td*6 + fmb*-2
-                elif position == "WR":
-                    rec_yds = float(items[12].text)
-                    rec_td = float(items[14].text)
-                    rush_yds = float(items[18].text)
-                    rush_td = float(items[20].text)
-                    fmb = float(items[23].text)
-                    points = rec_yds/10 + rec_td*6 + rush_yds/10 + rush_td*6 + fmb*-2
-                elif position == "RB":
-                    rush_yds = float(items[11].text)
-                    rush_td = float(items[13].text)
-                    rec_yds = float(items[16].text)
-                    rec_td = float(items[18].text)
-                    fmb = float(items[23].text)
-                    points = rush_yds/10 + rush_td*6 + rec_yds/10 + rec_td*6 + fmb*-2
-                elif position == "K":
-                    #points calculation
-                    xpm = float(items[10].text)
-                    xpa = float(items[11].text)
-                    fgm = float(items[13].text)
-                    fga = float(items[14].text)
-                    pts = float(items[17].text)
-                    points = (xpm- (xpa-xpm)) + (fgm*3 - (fga-fgm)) 
-                else:
-                    #wr/te/rb calc
-                    points = 0
-            except:
-                continue
-                   
+            points = calculatePoints(position, items)    
             append_row.append(points)
             sheet.append(append_row)
-            # sheet.append([points])
 
         
         dest_filename = firstName + "_" + lastName + year + ".xlsx"
         wb.save(filename = dest_filename)
-
-        
 
         count +=1
         

e2800e9d47e98583a234f2b1088b5db52e6c6707
saprap1 <priyasapra.2017@gmail.com>
2019-10-28 00:42:27 +0100

    clean up dead code and modularize code

--- read_through_names.py
+++ read_through_names.py
@@ -4,6 +4,9 @@
 Created on Wed Aug  7 10:57:56 2019
 
 @authors: Jay Messina, Priya Sapra
+
+@description: Scrape all the information for each player and store the data in spreadsheets
+                
 """
 import openpyxl
 import os
@@ -11,67 +14,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-'''
-    fantasy points calculation by position and returns the total calculated for each game
-    Since items is a list of <td> tags, we can just get the text from that item in the list
-'''
-def calculate_points(position, items):
-    points = 0
-    try:
-        if position == "QB":
-            yds_pass = float(items[13].text)
-            td_pass = float(items[14].text)
-            intercep = float(items[15].text)
-            rush_yds = float(items[22].text)
-            rush_td = float(items[24].text)
-            #fumb = float(items[34])
-            points = yds_pass/25 + td_pass*4 + intercep*-2 + rush_yds/10 + rush_td*6 
-            #fumb*-2
-        elif position == "TE":
-            # Is there a way to get the information by the attribute data-stat? that seems to be the only consistent thing among players
-            # Works for: Rob Gronkowski
-            # Fails for: Delanie Walker (only 1 row, not fumbles), Eric Ebron (has an entire section for passing unlike gronk), Ed Dickson (doesn't even have fumbles)
-            print(items)
-            print('yds_rec', items[10].text, 'td', items[12].text, 'fmb', items[17].text)
-            # print(items)
-            yds_rec = float(items[12].text)
-            td = float(items[14].text)
-            fmb = float(items[17].text)
-            points = yds_rec/10 + td*6 + fmb*-2
-        elif position == "WR":
-            rec_yds = float(items[12].text)
-            rec_td = float(items[14].text)
-            rush_yds = float(items[18].text)
-            rush_td = float(items[20].text)
-            fmb = float(items[23].text)
-            points = rec_yds/10 + rec_td*6 + rush_yds/10 + rush_td*6 + fmb*-2
-        elif position == "RB":
-            rush_yds = float(items[11].text)
-            rush_td = float(items[13].text)
-            rec_yds = float(items[16].text)
-            rec_td = float(items[18].text)
-            fmb = float(items[23].text)
-            points = rush_yds/10 + rush_td*6 + rec_yds/10 + rec_td*6 + fmb*-2
-        elif position == "K":
-            #points calculation
-            xpm = float(items[10].text)
-            xpa = float(items[11].text)
-            fgm = float(items[13].text)
-            fga = float(items[14].text)
-            pts = float(items[17].text)
-            points = (xpm- (xpa-xpm)) + (fgm*3 - (fga-fgm)) 
-        else:
-            #wr/te/rb calc
-            points = 0
-    except:
-        # print("skipping points calculation for...", firstName, lastName)
-        print("skip")
-        # continue
-    # print(points)
-    return points
-
-
-# Get the fantasy points for this player
+# Get the fantasy points for this player from the fantasy log online
 def get_fant_points(link_half, year):
 
     # It broke at https://www.pro-football-reference.com/players/S/SpenDi00/fantasy/2017 because no fantasy log exists
@@ -112,21 +55,14 @@ def get_fant_points(link_half, year):
 
     return ret_points
 
-
-
-'''
-- doesn't write dataframe into excel yet (have that stuff commented out rn)
-- currently set to run for 2018 only
-- have't tried running the whole thing entirely, so there might be bugs there (there's a thing with the arrays not being the same length...
-    i know you can fill those spots with something, but I can't figure that out)
-'''
-def main():
+# write the data for the specified year to excelt sheets
+def load_data(year):
+    
     book2 = openpyxl.Workbook()
     book2 = openpyxl.load_workbook('all_players_2019.xlsx')
-    sheet2 = book2.get_sheet_by_name('Sheet1')   
+    sheet2 = book2['Sheet1']
     row_count2 = sheet2.max_row
 
-    count = 0
 
     for i in range (2, row_count2+1):
 
@@ -136,8 +72,6 @@ def main():
         firstName = x[-1][1:]
         lastName = x[0]
         version = 0
-        year = "2018"
-        #year = "2017"
         
         if firstName == "Derek" and lastName == "Carr":
             version = 2
@@ -146,9 +80,9 @@ def main():
 
         
         ########################more debug stuff#######################
-        # link = "https://www.pro-football-reference.com/players/G/GronRo00/gamelog/2018/"
-        # firstName = "Rob"
-        # lastName = "Gronkowski"
+        link = "https://www.pro-football-reference.com/players/G/GronRo00/gamelog/2018/"
+        firstName = "Rob"
+        lastName = "Gronkowski"
 
         # link = "https://www.pro-football-reference.com/players/W/WalkHu00/gamelog/2018/"
         # firstName = "Delanie"
@@ -164,8 +98,8 @@ def main():
         # position = "TE"
         ###############################################################
 
-        #make soup request for data to formulate spreadsheet
-        #open a spreadsheet, add fields, save it
+        # make soup request for data to formulate spreadsheet
+        # open a spreadsheet, add fields, save it
         a = requests.get(link)
         soup = BeautifulSoup(a.text, 'lxml')
 
@@ -205,6 +139,9 @@ def main():
             continue
 
         data = []
+        
+        print(len(row), "games recorded for", year)
+        
         for x in row:
             data_append = []
 
@@ -237,6 +174,9 @@ def main():
         writer = pd.ExcelWriter(dest_path)
         df.to_excel(writer, 'Sheet1')
         writer.save()
-
-        
+    return
+ 
+def main():
+    load_data("2017")
+    load_data("2018")
 main()
